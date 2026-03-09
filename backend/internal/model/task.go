c5c0c40075a3617d6f79b80efd206e643c9c4457
xe-pc23 <xe.pc23@gmail.com>
2026-03-10 01:41:19 +0900

    add: Taskのモデルを作成

--- backend/internal/model/task.go
+++ backend/internal/model/task.go
@@ -1,1 +1,16 @@
 package model
+
+import "time"
+
+type Task struct {
+	ID             uint      `json:"id" gorm:"primaryKey"`
+	Title          string    `json:"title"`
+	Description    string    `json:"description"`
+	Status         string    `json:"status"`
+	Category       string    `json:"category"`
+	Priority       int       `json:"priority"`
+	StudyMinutes   int       `json:"study_minutes"`
+	AttachmentPath string    `json:"attachment_path"`
+	CreatedAt      time.Time `json:"created_at"`
+	UpdatedAt      time.Time `json:"updated_at"`
+}
