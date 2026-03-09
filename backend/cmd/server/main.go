7acb83ddb786b481da1aca68560fce113bc2d137
xe-pc23 <xe.pc23@gmail.com>
2026-03-10 01:29:50 +0900

    add: healthチェックコード

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,1 +1,10 @@
 package main
+
+import (
+	"study_quest/backend/internal/router"
+)
+
+func main() {
+	e := router.NewRouter()
+	e.Logger.Fatal(e.Start(":8080"))
+}
--- backend/internal/router/router.go
+++ backend/internal/router/router.go
@@ -1,1 +1,14 @@
 package router
+
+import "github.com/labstack/echo/v4"
+
+func NewRouter() *echo.Echo {
+	e := echo.New()
+
+	e.GET("/health", func(c echo.Context) error {
+		return c.JSON(200, map[string]string{
+			"message": "ok",
+		})
+	})
+	return e
+}
